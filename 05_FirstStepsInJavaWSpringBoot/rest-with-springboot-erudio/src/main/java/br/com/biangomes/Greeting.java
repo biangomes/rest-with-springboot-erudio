d2024cc12deaf7652d1579f4238b0d0d3c135cdc
biangomes <beanascigom@gmail.com>
2023-02-13 17:03:47 -0300

    Aula 50: 0508 - Enviando nosso código para o github

---
+++ 05_FirstStepsInJavaWSpringBoot/rest-with-springboot-erudio/src/main/java/br/com/biangomes/Greeting.java
@@ -0,0 +1,22 @@
+package br.com.biangomes;
+
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	
+	public Greeting(long id, String content) {
+		this.id=id;
+		this.content=content;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}
+	
+}
