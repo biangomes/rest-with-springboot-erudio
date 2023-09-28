819b2cd9accfda31d2fa0eb1b6bc2be1bd64d201
beatriz_gomes <beatriz.gomes@softplan.com.br>
2023-09-28 15:59:44 -0300

    Tratamento de exceções

--- 05_FirstStepsInJavaWSpringBoot/rest-with-springboot-erudio/src/main/java/br/com/biangomes/MathController.java
+++ 05_FirstStepsInJavaWSpringBoot/rest-with-springboot-erudio/src/main/java/br/com/biangomes/MathController.java
@@ -2,8 +2,10 @@ package br.com.biangomes;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.biangomes.exceptions.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.*;
+
+import static org.apache.tomcat.util.http.parser.HttpParser.isNumeric;
 
 @RestController
 public class MathController {
@@ -11,6 +13,34 @@ public class MathController {
 	private int n1;
 	private int n2;
 	private final AtomicLong counter = new AtomicLong();
-	
-	
+
+	@RequestMapping(value = "/sum/{n1}/{n2}", method = RequestMethod.GET)
+	public Double sum(@PathVariable("n1") String n1, @PathVariable("n2") String n2) throws Exception {
+
+		if (!isNumeric(n1) || !isNumeric(n2)) {
+			throw new UnsupportedMathOperationException("Não é permitido caracteres diferentes de números!");
+		}
+
+		return convertToDouble(n1) + convertToDouble(n2);
+
+	}
+
+	private Double convertToDouble(String numero) {
+		if (numero.equals(null)) return 0D;
+
+		// parser para usar o padrão do Brasil
+		String number = numero.replaceAll(",",".");
+
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String numero) {
+		if (numero.equals(null)) return false;
+
+		String number = numero.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+
+
 }
---
+++ 05_FirstStepsInJavaWSpringBoot/rest-with-springboot-erudio/src/main/java/br/com/biangomes/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,40 @@
+package br.com.biangomes.exceptions.handler;
+
+import br.com.biangomes.exceptions.ExceptionResponse;
+import br.com.biangomes.exceptions.UnsupportedMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest req) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                req.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest req) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                req.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
